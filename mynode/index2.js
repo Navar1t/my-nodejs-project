adc673619a87799105a237758a522550edd80d4f
Navar1t <655021000485@mail.rmutk.ac.th>
2024-09-24 13:46:06 +0700

    Initial commit

---
+++ mynode/index2.js
@@ -0,0 +1,7 @@
+// const fs = require("fs");
+// var msg = "Hello from node";
+
+// fs.writeFile("message.txt",msg, (err) => {
+//     if(err) throw err;
+//     console.log("The file has been savd!");
+// });
---
+++ sillyname/index.js
@@ -0,0 +1,9 @@
+var generateName = require('silly');
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
